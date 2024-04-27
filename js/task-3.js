6eb76594f608bb8241d5e1a691bea4ce863b3766
OLyaNag <olyasikaa@gmail.com>
2024-04-27 22:06:57 +0300

    hw4

---
+++ js/task-3.js
@@ -0,0 +1,23 @@
+'use strict'
+const profile = {
+    username: "Jacob",
+    changeUsername(newName) {
+        this.username = newName;
+    },
+    playTime: 300,
+    updatePlayTime(hours) { 
+     this.playTime += hours;
+    },
+    getInfo() {
+        return `${profile.username} has ${this.playTime} active hours!`;
+    },
+};
+
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
